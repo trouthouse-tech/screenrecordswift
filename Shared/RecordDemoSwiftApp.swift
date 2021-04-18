7e6a1711bede061cbfbdccdbffebfd595378ed53
mzruiz <matthew.z.ruiz@gmail.com>
2021-04-18 17:34:52 -0400

    Initial Commit

---
+++ Shared/RecordDemoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordDemoSwiftApp.swift
+//  Shared
+//
+//  Created by Matthew Ruiz on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RecordDemoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
